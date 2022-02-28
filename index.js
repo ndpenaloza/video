bd3930916236cff1470444c721c34b64e2149cf0
NDPenaloza <ndpenaloza@gmail.com>
2022-02-27 21:45:10 -0800

    added user and users.js in models and routes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +23,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
