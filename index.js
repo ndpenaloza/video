621e2aef923aa7ebc2f7f4daa6dc2b494915643c
NDPenaloza <ndpenaloza@gmail.com>
2022-02-28 00:56:13 -0800

    added auth.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,6 +25,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
