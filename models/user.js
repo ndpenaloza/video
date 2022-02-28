772f2e025168ea38ec886fae49b6cb4f4329b335
NDPenaloza <ndpenaloza@gmail.com>
2022-02-28 00:19:01 -0800

    fixed users routes, now passing data to db

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const User = mongoose.model('User', new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 5,
+    minlength: 2,
     maxlength: 50,
     trim: true,
   },
@@ -32,11 +32,11 @@ const User = mongoose.model('User', new mongoose.Schema({
 
 function validateUser(user) {
   const schema = Joi.object({
-    name: Joi.string(5).min(50).required(),
+    name: Joi.string().min(2).required(),
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max().required()
+    password: Joi.string().min(5).max(1024).required()
   })
-  return Joi.validate(user, schema);
+  return schema.validate(user);
 };
 
 module.exports.User = User;
