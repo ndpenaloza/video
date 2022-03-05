680622be8f7588a6ea632a309fa4085095efbeeb
NDPenaloza <ndpenaloza@gmail.com>
2022-03-05 01:33:58 -0800

    added middleware to check for tokens when modifying data, worked on jwt, config

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid');
 const mongoose = require('mongoose');
@@ -11,6 +12,11 @@ const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined');
+  process.exit(1);
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
