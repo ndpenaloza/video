7637d5b56df11de8986bc6a87358dbd12fe7365e
NDPenaloza <ndpenaloza@gmail.com>
2022-03-11 23:52:44 -0800

    last commit failed

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,13 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config')
 
 module.exports = function() {
-  mongoose.connect('mongodb://localhost/video', {
+  const db = config.get('db');
+  mongoose.connect(db, {
   useUnifiedTopology: true
   })
-  .then(() => console.log('Connected to MongoDB'))
-  .then(() => winston.info('Connected to MongoDB'))
+  .then(() => console.log(`Connected to ${db}`))
+  .then(() => winston.info(`Connected to ${db}`))
   .catch((err) => console.error('Could not connect to MongoDB', err))
 }
